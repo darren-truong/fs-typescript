a18bbdd89c64291abf3d52d1368cd5718f1aff6e
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2026-06-02 15:22:20 -0700

    Exercise: 1. Body mass index

---
+++ healthapp/bmiCalculator.ts
@@ -0,0 +1,26 @@
+function calculateBmi(height: number, weight: number): string {
+  const bmi = weight / ((height / 100) * (height / 100));
+  let message = "";
+
+  if (bmi >= 40.0) {
+    message = "Obese (Class III)";
+  } else if (bmi >= 35.0) {
+    message = "Obese (Class II)";
+  } else if (bmi >= 30.0) {
+    message = "Obese (Class I)";
+  } else if (bmi >= 25.0) {
+    message = "Overweight (Pre-obese)";
+  } else if (bmi >= 18.5) {
+    message = "Normal range";
+  } else if (bmi >= 17.0) {
+    message = "Underweight (Mild thinness)";
+  } else if (bmi >= 16.0) {
+    message = "Underweight (Moderate thinness)";
+  } else {
+    message = "Underweight (Severe thinness)";
+  }
+
+  return message;
+}
+
+console.log(calculateBmi(180, 74));
