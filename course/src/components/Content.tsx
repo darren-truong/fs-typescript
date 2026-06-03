ef663f448570edaee91c480b89afc7c1db976509
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2026-06-03 14:56:48 -0700

    Exercise: 17. Course, step 1

---
+++ course/src/components/Content.tsx
@@ -0,0 +1,17 @@
+type ContentProps = {
+  courseParts: { name: string; exerciseCount: number }[];
+};
+
+const Content = ({ courseParts }: ContentProps) => {
+  return (
+    <>
+      {courseParts.map((coursePart) => (
+        <p key={coursePart.name}>
+          {coursePart.name} {coursePart.exerciseCount}
+        </p>
+      ))}
+    </>
+  );
+};
+
+export default Content;
